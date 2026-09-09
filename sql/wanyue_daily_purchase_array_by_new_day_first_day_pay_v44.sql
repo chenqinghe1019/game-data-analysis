d898fa330ab63936bfa6c51b3b13c16e78ed8075
chenqinghe1019 <reclear1019@gmail.com>
2026-09-09 17:08:10 +0800

    弯月每日购买路径改为累计付费分层

--- sql/wanyue_daily_purchase_array_by_new_day_first_day_pay_v44.sql
+++ sql/wanyue_daily_purchase_array_by_new_day_first_day_pay_v44.sql
@@ -1,253 +1,294 @@
 SELECT
     row_number() OVER (
         ORDER BY
-            t."首日付费分层",
+            t."累计付费分层",
             t."玩家ID",
             t."新增天数"
     ) AS "序号",
 
-    t."首日付费分层",
+    t."累计付费分层",
     t."玩家ID",
     t."新增日期",
     t."新增天数",
     t."付费日期",
+    round(t."累计付费金额", 2) AS "累计付费金额",
     t."当天购买商品"
 
 FROM
 (
     SELECT
         CASE
-            WHEN x."首日付费金额" = 0
+            WHEN y."累计付费金额" = 0
                 THEN 'a_free'
-            WHEN x."首日付费金额" <= 6
+            WHEN y."累计付费金额" <= 6
                 THEN 'b_(0,6]'
-            WHEN x."首日付费金额" <= 30
+            WHEN y."累计付费金额" <= 30
                 THEN 'c_(6,30]'
-            WHEN x."首日付费金额" <= 100
+            WHEN y."累计付费金额" <= 100
                 THEN 'd_(30,100]'
-            WHEN x."首日付费金额" <= 300
+            WHEN y."累计付费金额" <= 300
                 THEN 'e_(100,300]'
-            WHEN x."首日付费金额" <= 500
+            WHEN y."累计付费金额" <= 500
                 THEN 'f_(300,500]'
-            WHEN x."首日付费金额" <= 1000
+            WHEN y."累计付费金额" <= 1000
                 THEN 'g_(500,1000]'
             ELSE 'h_(1000,+)'
-        END AS "首日付费分层",
+        END AS "累计付费分层",
 
-        x."#account_id" AS "玩家ID",
-        x."新增日期",
+        y."#account_id" AS "玩家ID",
+        y."新增日期",
 
         date_diff(
             'day',
-            x."新增日期",
-            x."付费日期"
+            y."新增日期",
+            y."付费日期"
         ) + 1 AS "新增天数",
 
-        x."付费日期",
-
-        array_agg(
-            x."商品名称"
-            ORDER BY
-                x."#event_time",
-                x."product_id"
-        ) AS "当天购买商品"
+        y."付费日期",
+        y."累计付费金额",
+        y."当天购买商品"
 
     FROM
     (
         SELECT
-            p."#account_id",
-            p."新增日期",
-            p."付费日期",
-            p."#event_time",
-            p."product_id",
-            p."商品名称",
+            d."#account_id",
+            d."新增日期",
+            d."付费日期",
+            d."当日付费金额",
+            d."当天购买商品",
 
-            sum(
-                CASE
-                    WHEN p."付费日期" = p."新增日期"
-                        THEN p."付费金额"
-                    ELSE 0
-                END
-            ) OVER (
+            sum(d."当日付费金额") OVER (
                 PARTITION BY
-                    p."#account_id",
-                    p."新增日期"
-            ) AS "首日付费金额"
+                    d."#account_id",
+                    d."新增日期"
+                ORDER BY
+                    d."付费日期"
+                ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+            ) AS "累计付费金额"
 
         FROM
         (
             SELECT
-                u."#account_id",
-                u."新增日期",
-                e."付费日期",
-                e."#event_time",
-                e."product_id",
-                e."付费金额",
+                p."#account_id",
+                p."新增日期",
+                p."付费日期",
 
-                coalesce(
-                    d."name",
-                    concat(
-                        '未配置商品_',
-                        e."product_id"
-                    )
-                ) AS "商品名称"
+                sum(
+                    p."付费金额"
+                ) AS "当日付费金额",
+
+                array_agg(
+                    p."商品名称"
+                    ORDER BY
+                        p."#event_time",
+                        p."product_id"
+                ) AS "当天购买商品"
 
             FROM
             (
                 SELECT
-                    u1."#account_id",
-                    u1."新增日期"
+                    u."#account_id",
+                    u."新增日期",
+
+                    e."付费日期",
+                    e."#event_time",
+                    e."product_id",
+                    e."付费金额",
+
+                    coalesce(
+                        d."name",
+                        concat(
+                            '未配置商品_',
+                            e."product_id"
+                        )
+                    ) AS "商品名称"
 
                 FROM
                 (
                     SELECT
-                        u0."#account_id",
-                        u0."新增日期",
-                        cast(
-                            u0."新增日期"
-                            AS varchar
-                        ) AS "$part_date"
+                        u1."#account_id",
+                        u1."新增日期"
 
                     FROM
                     (
                         SELECT
-                            cast(
-                                v."#account_id"
-                                AS varchar
-                            ) AS "#account_id",
+                            u0."#account_id",
+                            u0."新增日期",
 
-                            min(
-                                coalesce(
-                                    date(
-                                        try_cast(
-                                            cast(
-                                                v."create_role_time"
-                                                AS varchar
-                                            )
-                                            AS timestamp
-                                        )
-                                    ),
-                                    date(
-                                        from_unixtime(
+                            cast(
+                                u0."新增日期"
+                                AS varchar
+                            ) AS "$part_date"
+
+                        FROM
+                        (
+                            SELECT
+                                cast(
+                                    v."#account_id"
+                                    AS varchar
+                                ) AS "#account_id",
+
+                                min(
+                                    coalesce(
+                                        date(
                                             try_cast(
                                                 cast(
                                                     v."create_role_time"
                                                     AS varchar
                                                 )
-                                                AS double
+                                                AS timestamp
+                                            )
+                                        ),
+
+                                        date(
+                                            from_unixtime(
+                                                try_cast(
+                                                    cast(
+                                                        v."create_role_time"
+                                                        AS varchar
+                                                    )
+                                                    AS double
+                                                )
                                             )
                                         )
                                     )
+                                ) AS "新增日期"
+
+                            FROM ta.v_user_44 v
+
+                            WHERE v."domain" = 'release'
+                              AND v."#account_id" IS NOT NULL
+                              AND v."create_role_time" IS NOT NULL
+
+                            GROUP BY
+                                cast(
+                                    v."#account_id"
+                                    AS varchar
                                 )
-                            ) AS "新增日期"
+                        ) u0
 
-                        FROM ta.v_user_44 v
+                        WHERE u0."新增日期" IS NOT NULL
+                          AND u0."新增日期" <= current_date
+                    ) u1
 
-                        WHERE v."domain" = 'release'
-                          AND v."#account_id" IS NOT NULL
-                          AND v."create_role_time" IS NOT NULL
+                    WHERE ${PartDate:date1}
+                ) u
 
-                        GROUP BY
+                INNER JOIN
+                (
+                    SELECT
+                        cast(
+                            e0."#account_id"
+                            AS varchar
+                        ) AS "#account_id",
+
+                        date(
+                            e0."#event_time"
+                        ) AS "付费日期",
+
+                        date(
+                            e0."$part_date"
+                        ) AS "分区日期",
+
+                        e0."#event_time",
+
+                        coalesce(
                             cast(
-                                v."#account_id"
+                                try_cast(
+                                    e0."product_id"
+                                    AS bigint
+                                )
+                                AS varchar
+                            ),
+                            cast(
+                                e0."product_id"
                                 AS varchar
                             )
-                    ) u0
+                        ) AS "product_id",
 
-                    WHERE u0."新增日期" IS NOT NULL
-                      AND u0."新增日期" <= current_date
-                ) u1
+                        coalesce(
+                            try_cast(
+                                e0."payment"
+                                AS double
+                            ),
+                            0
+                        ) / 100.0000 AS "付费金额"
 
-                WHERE ${PartDate:date1}
-            ) u
+                    FROM ta.v_event_44 e0
 
-            INNER JOIN
-            (
-                SELECT
-                    cast(
-                        e0."#account_id"
-                        AS varchar
-                    ) AS "#account_id",
+                    WHERE e0."$part_event" = 'pay_log'
+                      AND e0."#account_id" IS NOT NULL
+                      AND e0."#event_time" IS NOT NULL
+                      AND e0."$part_date" IS NOT NULL
+                      AND date(e0."$part_date") <= current_date
 
-                    date(
-                        e0."#event_time"
-                    ) AS "付费日期",
+                      AND coalesce(
+                            try_cast(
+                                e0."payment"
+                                AS double
+                            ),
+                            0
+                          ) > 0
+                ) e
+                    ON e."#account_id" = u."#account_id"
+                   AND e."付费日期" >= u."新增日期"
+                   AND e."分区日期" >= u."新增日期"
 
-                    date(
-                        e0."$part_date"
-                    ) AS "分区日期",
-
-                    e0."#event_time",
-
-                    cast(
-                        e0."product_id"
-                        AS varchar
-                    ) AS "product_id",
-
-                    coalesce(
-                        try_cast(
-                            e0."payment"
-                            AS double
-                        ),
-                        0
-                    ) / 100.0000 AS "付费金额"
-
-                FROM ta.v_event_44 e0
-
-                WHERE e0."$part_event" = 'pay_log'
-                  AND e0."#account_id" IS NOT NULL
-                  AND e0."#event_time" IS NOT NULL
-                  AND e0."$part_date" IS NOT NULL
-                  AND date(e0."$part_date") <= current_date
-                  AND coalesce(
-                        try_cast(
-                            e0."payment"
-                            AS double
-                        ),
-                        0
-                      ) > 0
-            ) e
-                ON e."#account_id" = u."#account_id"
-               AND e."付费日期" >= u."新增日期"
-               AND e."分区日期" >= u."新增日期"
-
-            LEFT JOIN
-            (
-                SELECT
-                    cast(
-                        "product_id"
-                        AS varchar
-                    ) AS "product_id",
-
-                    max(
-                        trim(
+                LEFT JOIN
+                (
+                    SELECT
+                        coalesce(
                             cast(
-                                "name"
+                                try_cast(
+                                    "product_id"
+                                    AS bigint
+                                )
+                                AS varchar
+                            ),
+                            cast(
+                                "product_id"
+                                AS varchar
+                            )
+                        ) AS "product_id",
+
+                        max(
+                            trim(
+                                cast(
+                                    "name"
+                                    AS varchar
+                                )
+                            )
+                        ) AS "name"
+
+                    FROM ta_ext.product_id_44
+
+                    GROUP BY
+                        coalesce(
+                            cast(
+                                try_cast(
+                                    "product_id"
+                                    AS bigint
+                                )
+                                AS varchar
+                            ),
+                            cast(
+                                "product_id"
                                 AS varchar
                             )
                         )
-                    ) AS "name"
+                ) d
+                    ON e."product_id" = d."product_id"
+            ) p
 
-                FROM ta_ext.product_id_44
-
-                GROUP BY
-                    cast(
-                        "product_id"
-                        AS varchar
-                    )
-            ) d
-                ON e."product_id" = d."product_id"
-        ) p
-    ) x
-
-    GROUP BY
-        x."#account_id",
-        x."新增日期",
-        x."付费日期",
-        x."首日付费金额"
+            GROUP BY
+                p."#account_id",
+                p."新增日期",
+                p."付费日期"
+        ) d
+    ) y
 ) t
 
 ORDER BY
-    t."首日付费分层",
+    t."累计付费分层",
     t."玩家ID",
     t."新增天数";
