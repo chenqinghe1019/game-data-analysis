63b4ec3df1c8fc0fa1f99166aeefeb313599dfbe
chenqinghe1019 <reclear1019@gmail.com>
2026-09-10 18:05:53 +0800

    统一成熟版LTV精度为4位

--- sql/xiafangle_payment_item_ltv_contribution_d1_d30_v41.sql
+++ sql/xiafangle_payment_item_ltv_contribution_d1_d30_v41.sql
@@ -38,13 +38,13 @@ SELECT
     round(
         x."当日付费金额" * 1.0000
         / nullif(x."成熟新增人数", 0),
-        2
+        4
     ) "当日LTV贡献",
 
     round(
         x."当日总付费金额" * 1.0000
         / nullif(x."成熟新增人数", 0),
-        2
+        4
     ) "当日总LTV",
 
     coalesce(
@@ -62,13 +62,13 @@ SELECT
     round(
         x."累计付费金额" * 1.0000
         / nullif(x."成熟新增人数", 0),
-        2
+        4
     ) "累计LTV贡献",
 
     round(
         x."累计总付费金额" * 1.0000
         / nullif(x."成熟新增人数", 0),
-        2
+        4
     ) "累计总LTV",
 
     coalesce(
